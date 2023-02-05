9045625d74eccd2ad36e9edbeb7ee30edb4cb8a1
deabreudavid <de_abreu.david@yahoo.fr>
2023-02-05 22:26:56 +0100

    fonctions

---
+++ 05-Révisions-JS-LiterauxDeGabarit/app.js
@@ -0,0 +1,18 @@
+//sans les itéraux de gabarit
+
+const name = "david";
+const age = 30;
+const job = "dev";
+
+const hero = "i am " + name + " and i have " + age + " and i am " + job;
+
+console.log(hero);
+
+//avec les itéraux de gabarit depuis l'ES6
+const name2 = "fred";
+const age2 = 32;
+const job2 = "developer";
+
+const hero2 = `i am ${name2}, i have ${age2} and i am ${job2}`;
+
+console.log(hero2);
---
+++ 06-Révisions-JS-Selecteurs/app.js
@@ -0,0 +1,16 @@
+const titre = document.querySelector("h1");
+// const titre = document.querySelector(".nameClass");
+// const titre = document.querySelector("#nameId");
+console.log(titre);
+
+const items = document.querySelectorAll("li");
+console.log(items);
+
+const text = document.getElementsByClassName("text");
+console.log(text);
+
+const tile = document.getElementById("title");
+console.log(tile);
+
+const el = document.getElementsByTagName("a");
+console.log(el);
