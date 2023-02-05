1e587c5134743cfa3c4cc642da2ac5d539b06ea9
deabreudavid <de_abreu.david@yahoo.fr>
2023-02-05 21:45:00 +0100

    values

--- 03-Révisions-JS-typesDeValeur/app.js
+++ 03-Révisions-JS-typesDeValeur/app.js
@@ -1,3 +1,28 @@
 //string
-let txt ="david";
-let txt="fred"
+let txt = "david";
+let txt = "fred";
+let txt = "michel";
+
+//number
+let nb = 1;
+let nb2 = 2;
+
+// boolean
+let bool = false;
+let bool2 = true;
+
+//table
+let tab= [1,2,3];
+let tab2= ["david", "fred", "michel"];
+
+//objets
+let obj ={
+    name: "david",
+    age: 20,
+}
+
+//undefined
+let und;
+
+//null
+let nul = null;
