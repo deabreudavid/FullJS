4d50d34c7d95f460d5f88a8d2fd1e05df86842ab
deabreudavid <de_abreu.david@yahoo.fr>
2023-03-31 09:36:36 +0200

    18 19 conditions switch parents/child

---
+++ Partie3/17-If-Else/app.js
@@ -0,0 +1,9 @@
+let nb = 5;
+
+if (nb < 5 && nb === 5) {
+  console.log("first condition ok");
+} else if(nb > 5) {
+  console.log("second condition ok");
+}else{
+    console.log("third condition ok")
+}
---
+++ Partie3/18-conditions-switch/app.js
@@ -0,0 +1,15 @@
+let David = "blue";
+
+switch(David){
+    case "blue":
+        console.log("50 euros");
+        break;
+    case "red":
+        console.log("10 euros");
+        break;
+    case "green":
+        console.log("free");
+        break;
+    default:
+        console.log("out of stock")
+}
